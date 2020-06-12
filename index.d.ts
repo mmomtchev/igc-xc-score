70609b76e0daf3481ef308a75eede406a06c04ac
Momtchil Momtchev <momtchil@momtchev.com>
2020-06-12 20:41:57 +0200

    full return type for TypeScript

--- index.d.ts
+++ index.d.ts
@@ -2,5 +2,45 @@ export as namespace XCScoring;
 
 import { IGCFile } from 'igc-parser';
 
-export function solver(flight: IGCFile, scoringRules: object, config: { [key: string]: any });
+interface Point {
+    x: number
+    y: number
+    r: number
+};
+
+interface ClosingPoints {
+    d: number,
+    in: Point,
+    out: Point
+};
+
+interface ScoreInfo {
+    cp?: ClosingPoints,
+    tp?: Point[3],
+    distance: number,
+    score: number
+};
+
+interface Scoring {
+    name: string;
+    code: string
+};
+
+interface Opt {
+    scoring: Scoring
+};
+
+interface Solution {
+    bound: number;
+    currentUpperBound: number,
+    id: number|string,
+    opt: Opt,
+    optimal?: boolean,
+    processed?: number,
+    score?: number,
+    scoreInfo?: ScoreInfo,
+    time?: number
+};
+
+export function solver(flight: IGCFile, scoringRules: object, config: { [key: string]: any }) : Iterator<Solution, Solution>;
 export const scoringRules: { [key: string]: object[]; };
