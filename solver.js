36ff8fce560bf749fa8064e476f3adcb7373f161
Momtchil Momtchev <momtchil@momtchev.com>
2021-02-23 10:35:52 +0100

    greatly simplify the crash reproduction

--- solver.js
+++ solver.js
@@ -8,25 +8,13 @@
  */
 const WorkerThreads = require('worker_threads');
 const Worker = WorkerThreads.Worker;
-const SortedSet = require('collections/sorted-set');
 const worker = require('./worker');
 const Solution = require('./solution').Solution;
 const { Range, Point, Box } = require('./foundation');
-const scoringRules = require('./scoring-rules.config');
 
 const NWORKERS = require('os').cpus().length;
-const MAXQ = 40;
 const MINQ = 20;
 
-function enqueueSolution(s) {
-    this.postMessage(s);
-    this.qlen++;
-    if (this.q === undefined) {
-        this.q = new Promise((res) => {
-            this.resolve = res;
-        });
-    }
-}
 
 function dequeueSolutions(msgs) {
 /* This is the ugly part of JS */
@@ -69,35 +57,11 @@ function dequeueSolutions(msgs) {
  * @param {boolean=} config.trim automatically detect launch and landing and trim the flight track, default false
  */
 async function* solver(flight, _scoringRules, _config) {
-    let reset;
-
     const rules = _scoringRules || 'FFVL';
     const config = _config || {};
     config.flight = flight;
     config.rules = rules;
     worker.prepare(config);
-    let solutionRoots = [];
-    for (let scoringType in scoringRules[rules]) {
-        for (let l of flight.ll) {
-            const opt = {
-                launch: l.launch,
-                landing: l.landing,
-                scoringType: scoringType
-            };
-            let solutionRoot = new Solution([
-                new Range(l.launch, l.landing),
-                new Range(l.launch, l.landing),
-                new Range(l.launch, l.landing)
-            ], opt, config);
-            solutionRoot.do_bound(config);
-            solutionRoot.do_score(config);
-            solutionRoots.push(solutionRoot);
-        }
-    }
-
-    let best = solutionRoots[0];
-    let solutionQueue = new SortedSet(solutionRoots, Solution.prototype.contentEquals, Solution.prototype.contentCompare);
-    let processed = 0;
 
     //delete flight.errors;
     flight.errors = new Error('err');
@@ -111,100 +75,7 @@ async function* solver(flight, _scoringRules, _config) {
             workers[w].results = [];
         }
     }
-
-    let tcum = 0;
-    do {
-        const tstart = Date.now();
-        while (solutionQueue.length > 0 || (workers && workers.filter(x => x.q).length > 0)) {
-            if (processed % 100 === 0) {
-                if (typeof process !== 'undefined' && process.memoryUsage) {
-                    const mem = process.memoryUsage();
-                    if (mem.heapUsed / mem.heapTotal > 0.98)
-                        break;
-                }
-            }
-
-            if (solutionQueue.length > 0 && workers && workers.filter(x => x.q).length < NWORKERS) {
-                let current = solutionQueue.pop();
-                if (current.scoring(config).rounding(current.bound) <= best.scoring(config).rounding(best.score)) {
-                    solutionQueue.clear();
-                    continue;
-                }
-                const w = workers.find(x => x.qlen < MAXQ);
-                current.trace('enqueue', config);
-                enqueueSolution.call(w, current);
-                processed++;
-            }
-
-            if (workers && workers.filter(x => x.q).length == NWORKERS || solutionQueue.length == 0) {
-                const w = await Promise.race(workers.filter(x => x.q).map(x => x.q));
-                for (let r of w.results) {
-                    r.trace('dequeue', config);
-                    if (r.score > best.score)
-                        best = r;
-                    if (r.bound <= best.score)
-                        continue;
-                    solutionQueue.push(r);
-                }
-                w.results = [];
-            }
-
-            if (!workers) {
-                let current = solutionQueue.pop();
-                if (current.scoring(config).rounding(current.bound) <= best.scoring(config).rounding(best.score)) {
-                    solutionQueue.clear();
-                    continue;
-                }
-                let children = current.do_branch(config);
-                for (let c of children) {
-                    c.do_bound(config);
-                    c.do_score(config);
-                    if (c.score > best.score)
-                        best = c;
-                    if (c.bound <= best.score)
-                        continue;
-                    solutionQueue.push(c);
-                }
-            }
-
-            if (solutionQueue.length > 10000 && solutionQueue.findLeast().value.bound <= best.score) {
-                const garbageBest = solutionQueue.findGreatestLessThanOrEqual({ bound: best.score });
-                if (garbageBest !== undefined) {
-                    const cutoff = solutionQueue.indexOf(garbageBest.value);
-                    solutionQueue.splice(0, cutoff + 1).length;
-                }
-            }
-
-            if (processed > config.maxloop || (Date.now() - tstart) > config.maxcycle) {
-                break;
-            }
-        }
-
-        best.processed = processed;
-        const currentUpperBound = solutionQueue.findGreatest();
-        best.currentUpperBound = currentUpperBound ? currentUpperBound.value.bound : best.bound;
-        tcum += Date.now() - tstart;
-        best.time = tcum;
-        if (solutionQueue.length == 0)
-            best.optimal = true;
-        else
-            best.optimal = false;
-
-        if (best.optimal) {
-            best.score = best.scoring().rounding(best.score);
-            if (best.scoreInfo) {
-                best.scoreInfo.distance = best.scoring().rounding(best.scoreInfo.distance);
-                if (best.scoreInfo.cp)
-                    best.scoreInfo.cp.d = best.scoring().rounding(best.scoreInfo.cp.d);
-            }
-            reset = true;
-            if (config.env && config.env.WorkerThreads)
-                for (let w of workers)
-                    w.terminate();
-            return best;
-        } else
-            reset = yield best;
-    } while (!reset);
+    yield null;
 }
 
 module.exports = solver;
--- test.js
+++ test.js
@@ -1,10 +1,8 @@
 const fs = require('fs');
 const path = require('path');
 const WorkerThreads = require('worker_threads');
-const crypto = require('crypto');
 const IGCParser = require('igc-parser');
 const solver = require('./solver');
-const util = require('./util');
 
 const defaultConfig = {
     quiet: true,
@@ -15,37 +13,6 @@ const defaultConfig = {
 const tests = {
     FFVL: [
         { file: 'bigflat.igc', score: 224.09 },
-        { file: 'd3p.igc', score: 60.77 },
-        { file: 'fai.igc', score: 228.71, md5: '415ac5e167584bbcf93641fcbec7bbbb' },
-        { file: 'fai.igc', score: 228.72, config: { hp: true } },
-        { file: 'line.igc', score: 53.34, md5: 'f34917458bb110ca34be92439a2ca9de' },
-        { file: 'tri.igc', score: 17.51 },
-        { file: 'record_de_france.igc', score: 422.02 },
-        { file: 'record_de_france.igc', score: 421.99, config: { hp: true } },
-        { file: 'zigzag.igc', score: 90.64 },
-        { file: 'flat-ffvl-26.37.igc', score: 26.4 },
-        { file: 'curvature-of-earth-triangle.igc', score: 90.71 },
-        { file: 'vincentys.igc', score: 174.35 },
-        { file: 'discontinuity.igc', score: 53.01 },
-        { file: 'hiking-up.igc', score: 40.53 },
-        { file: 'hiking-up-2.igc', score: 125.94 },
-        { file: 'lunch-break.igc', score: 73.92 },
-        { file: 'dup-fixes.igc', score: 89.1 }
-    ],
-    XContest: [
-        { file: 'flat-xcontest-106.82.igc', score: 107.12 },
-        { file: 'trifai-xcontest-362.70.igc', score: 363.43 },
-        { file: 'trifai-xcontest-362.70.igc', score: 363.42, config: { hp: true } },
-        { file: 'freeflight-xcontest-66.46.igc', score: 66.5 },
-        { file: 'flat-xcontest-100.59.igc', score: 100.76 },
-        { file: 'flat-xcontest-8.01.igc', score: 8.01 },
-        { file: 'trifai-xcontest-189.65.igc', score: 189.96 },
-        { file: 'trifai-xcontest-189.65.igc', score: 189.96, config: { hp: true } },
-        { file: 'trifai-xcontest-452.21.igc', score: 453.04 },
-        { file: 'trifai-xcontest-452.21.igc', score: 453.02, config: { hp: true } },
-        { file: 'trifai-xcontest-307.57.igc', score: 308.26 },
-        { file: 'freeflight-xcontest-465.33.igc', score: 465.58 },
-        { file: 'opentri-xcontest-428.31.igc', score: 428.59 }
     ]
 };
 
@@ -53,33 +20,6 @@ const tests = {
     for (let rules of Object.keys(tests))
         for (let test of tests[rules]) {
             const flight = IGCParser.parse(fs.readFileSync(path.join('test', test.file), 'utf8'), { lenient: true });
-            const ts = Date.now();
-            const best = (await solver(flight, rules, { ...defaultConfig, ...test.config }).next()).value;
-            if (best.score == test.score)
-                console.log(rules, test.file,
-                    (test.config || {}).hp ? 'HP' : 'Fast', best.score,
-                    (Date.now() - ts) + 'ms',
-                    util.consoleColors.fg.green + String.fromCodePoint(0x2713) + util.consoleColors.reset);
-            else {
-                console.error(rules, test.file, test.score, best.score,
-                    util.consoleColors.fg.red + 'x' + util.consoleColors.reset);
-                if (process.argv[2] !== 'force')
-                    process.exit(1);
-            }
-            if (test.md5) {
-                const hash = crypto.createHash('md5');
-                const geojson = best.geojson(flight);
-                /* These are timing-dependent when multithreading */
-                delete geojson.properties.processedTime;
-                delete geojson.properties.processedSolutions;
-                delete geojson.properties.id;
-                hash.update(JSON.stringify(geojson));
-                const digest = hash.digest('hex');
-                if (digest !== test.md5) {
-                    console.error(rules, test.file, digest, test.md5);
-                    if (process.argv[2] !== 'force')
-                        process.exit(1);
-                }
-            }
+            (await solver(flight, rules, defaultConfig).next()).value;
         }
 })();
