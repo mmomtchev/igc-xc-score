33a72e8a315ab19ba564e3dcc21c95bc2cdc7c91
Momtchil Momtchev <momtchil@momtchev.com>
2021-02-23 11:02:56 +0100

    further simplify

--- module.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-export { solver, scoringRules, IGCParser };
-const scoringRules = require('./scoring-rules.config');
-const solver = require('./solver');
-const IGCParser = require('./igc-parser');					/* Obsolete, to be deleted in 1.6 */
--- test.js
+++ test.js
@@ -2,7 +2,10 @@ const fs = require('fs');
 const path = require('path');
 const WorkerThreads = require('worker_threads');
 const IGCParser = require('igc-parser');
-const solver = require('./solver');
+const Worker = WorkerThreads.Worker;
+const SharedMap = require('sharedmap');
+
+const NWORKERS = require('os').cpus().length;
 
 const defaultConfig = {
     quiet: true,
@@ -10,16 +13,28 @@ const defaultConfig = {
     env: { fs, WorkerThreads }
 };
 
-const tests = {
-    FFVL: [
-        { file: 'bigflat.igc', score: 224.09 },
-    ]
-};
+const test = { file: 'bigflat.igc', score: 224.09 };
+
+async function* solver(flight, _scoringRules, _config) {
+    const config = _config || {};
+    config.flight = flight;
+    config.flight.furthestPoints = [new SharedMap(65536, 48, 16), new SharedMap(65536, 48, 16)];
+
+    //delete flight.errors;
+    flight.errors = new Error('err');
+    let workers;
+    if (config.env && config.env.WorkerThreads) {
+        workers = new Array(NWORKERS).fill(undefined);
+        for (let w in workers) {
+            workers[w] = new Worker('./worker.js', { workerData: { flight } });
+            workers[w].qlen = 0;
+            workers[w].results = [];
+        }
+    }
+    yield null;
+}
 
 (async () => {
-    for (let rules of Object.keys(tests))
-        for (let test of tests[rules]) {
-            const flight = IGCParser.parse(fs.readFileSync(path.join('test', test.file), 'utf8'), { lenient: true });
-            (await solver(flight, rules, defaultConfig).next()).value;
-        }
+    const flight = IGCParser.parse(fs.readFileSync(path.join('test', test.file), 'utf8'), { lenient: true });
+    (await solver(flight, 'FFVL', defaultConfig).next()).value;
 })();
